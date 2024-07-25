8e08fcb27763003962aca999c0d3a03595688e1a
yunsseong <86508300+yunsseong@users.noreply.github.com>
2024-07-25 16:24:26 +0900

    [Silver I] Title: 회의실 배정, Time: 444 ms, Memory: 130924 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.py
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -1,17 +1,15 @@
 n = int(input())
 time = []
-max_time = 0
 
 for _ in range(n):
     time.append(list(map(int, input().split())))
 
 time.sort(key=lambda x: (x[1], x[0]))
-time_able = []
 cnt = 0
-max_cursor = 0
+cursor = 0
 for start, end in time:
-    if start >= max_cursor:
-        max_cursor = end
+    if start >= cursor:
+        cursor = end
         cnt += 1
 
 print(cnt)
